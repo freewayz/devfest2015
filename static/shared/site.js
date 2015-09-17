18f5fd80ef189f008a30ad4cbf4b828915b5bd8b
peter <fr33wayz@gmail.com>
2015-09-17 19:15:13 +0100

    Added Speakers Card images

--- static/shared/site.js
+++ static/shared/site.js
@@ -17,7 +17,9 @@ function animateToSection(link, current, previous) {
     header.appendChild(effectNode);
 
     var newColor = 'hsl(' + Math.round(Math.random() * 255) + ', 46%, 42%)';
-    effectNode.style.background = newColor;
+    var imageBackground = "devfest1"+1;
+    //effectNode.style.background = newColor;
+    effectNode.style.backgroundImage = imageBackground;
 
     var scaleSteps = [{transform: 'scale(0)'}, {transform: 'scale(1)'}];
     var timing = {duration: 2500, easing: 'ease-in-out'};
